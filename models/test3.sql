ecf4e74ca05ebec2f9b9de28bcb951eafb106d76
Niharika Sannametla <niharika.sannametla@ing.com>
2025-09-16 12:46:45 +0000

    sql

---
+++ models/test3.sql
@@ -0,0 +1,7 @@
+SELECT
+    1 AS id,
+    2 AS column_2,
+    3 AS column_3,
+    4 AS column_4
+FROM
+    abc
---
+++ models/test_4.sql
@@ -0,0 +1,11 @@
+SELECT
+1 as id,
+2 as    column_2, 
+3 as column_3,
+ 4 as column_4 ,
+  5 as column_5 ,
+   6 as column_6 ,
+7 as column_7 
+ from 
+ abc
+ 
