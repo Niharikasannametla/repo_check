72553599fc7f39efabf93499a2fa4ff7c8fca99d
Niharika Sannametla <niharika.sannametla@ing.com>
2025-10-16 05:30:54 +0000

    ok

--- macros/key_table_load_v1.sql
+++ macros/key_table_load_v1.sql
@@ -1,18 +1,18 @@
-{% macro key_table_load_v1(relations,model_name) %}
-    {% set re = modules.re %}
-    {% set statements = [] %}
-    {% set ns = namespace(counter=0) %}
-    {% for relation in relations %}
-        {% set keytable = model_name %}
-        {% set parts = keytable.split('_') %}
-        {% set keyconcept = parts[-2] | upper  %}
-        {% set pattern = "^"~ keyconcept ~ ".*SUP_KEY$" %}
-        {% set cols = adapter.get_columns_in_relation(relation) %}
-        {% for col in cols %}
-            {% set is_match = re.match(pattern, col.name, re.IGNORECASE) %}
-            {% if is_match %}
-                {% set columnname = col.name %}
-                {% set statement %}
+{%- macro key_table_load_v1(relations,model_name) -%}
+    {%- set re = modules.re -%}
+    {%- set statements = [] -%}
+    {%- set ns = namespace(counter=0) -%}
+    {%- for relation in relations -%}
+        {%- set keytable = model_name -%}
+        {%- set parts = keytable.split('_') -%}
+        {%- set keyconcept = parts[-2] | upper  -%}
+        {%- set pattern = "^"~ keyconcept ~ ".*SUP_KEY$" -%}
+        {%- set cols = adapter.get_columns_in_relation(relation) -%}
+        {%- for col in cols -%}
+            {%- set is_match = re.match(pattern, col.name, re.IGNORECASE) -%}
+            {%- if is_match -%}
+                {%- set columnname = col.name -%}
+                {%- set statement -%}
                     select
                         farm_fingerprint(v_{{ ns.counter }}.{{ keyconcept }}_sup_key) as {{ keyconcept }}_id,
                         v_{{ ns.counter }}.{{ keyconcept }}_sup_key,
@@ -30,28 +30,27 @@
                             split({{ columnname }}, '|')[offset(1)] as obj_tp_cd,
                             split({{ columnname }}, '|')[offset(0)] as mstr_src_stm_cd,
                             split({{ columnname }}, '|')[offset(2)] as UNQ_ID_IN_SRC_STM
-
                         from {{ relation }}
-                    ) v_{{ ns.counter }}
+                    ) v_{{ ns.counter }} 
                     left join {{ this }} k
                         on v_{{ ns.counter }}.{{ keyconcept }}_sup_key = k.{{ keyconcept }}_sup_key
-                    where k.{{ keyconcept }}_sup_key is null and v_{{ ns.counter }}.{{ keyconcept }}_sup_key is not null
-                {% endset %}
-                {% do statements.append(statement) %}
-                {% set ns.counter = ns.counter + 1 %}
-            {% endif %}
-        {% endfor %}
-    {% endfor %}
-    {% if statements | length > 0 %}
+                    where k.{{ keyconcept }}_sup_key is null and v_{{ ns.counter }}.{{ keyconcept }}_sup_key is not null 
+                {%- endset -%}
+                {%- do statements.append(statement) -%}
+                {%- set ns.counter = ns.counter + 1 -%}
+            {%- endif -%}
+        {%- endfor -%}
+    {%- endfor -%}
+    {%- if statements | length > 0 -%}
         select *
         from
             (
-                {% for stmt in statements %}
+                {%- for stmt in statements -%}
                     {% if not loop.first %}
                         union all
                     {% endif %}
                     {{ stmt }}
-                {% endfor %}
+                {% endfor -%}
                 {{ ")" }}
-    {% endif %}
-{% endmacro %}
+    {%- endif -%}
+{%- endmacro -%}
--- models/ip_k.sql
+++ models/ip_k.sql
@@ -5,14 +5,14 @@
     )
 }}
 
-{% set model_name = this.identifier %}
-{% set parts = model_name.split("_") %}
-{% set keyconcept = parts[-2] ~ "_SUP_KEY" | upper %}
-{% do log("keyconcept: " ~ keyconcept, info=True) %}
+{%- set model_name = this.identifier -%}
+{%- set parts = model_name.split("_") -%}
+{%- set keyconcept = parts[-2] ~ "_SUP_KEY" | upper -%}
+{%- do log("keyconcept: " ~ keyconcept, info=True) -%}
 
-{% set models = var("pm_table_name") %}
-{% do log("Models: " ~ models, info=True) %}
-{% set resolved_models = [] %}
-{% for m in models %} {% do resolved_models.append(ref(m)) %} {% endfor %}
+{%- set models = var("pm_table_name") -%}
+{%- do log("Models: " ~ models, info=True) -%}
+{%- set resolved_models = [] -%}
+{%- for m in models -%} {%- do resolved_models.append(ref(m)) -%} {%- endfor -%}
 
-{{ key_table_load_v1(resolved_models, model_name) }}
+{{- key_table_load_v1(resolved_models, model_name) -}}
