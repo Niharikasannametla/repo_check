1ceeec949296651b7c80c4f3ff68ee48b0f1c779
Niharika Sannametla <niharika.sannametla@ing.com>
2025-11-06 07:49:45 +0000

    ok

--- models/check/test4.sql
+++ models/check/test4.sql
@@ -2,5 +2,5 @@ select
     2 as column_2,
     3 as column_3,
     4 as column_4,
-    farm_fingerprint('{{ invocation_id }}') as Job_run_Id,
-    {{ env_var('DBT_CLOUD_RUN_ID',0) }} as column_6
+    farm_fingerprint('{{ invocation_id }}') as job_run_id,
+    {{ env_var('DBT_CLOUD_RUN_ID',0) }} as flow_run_id
