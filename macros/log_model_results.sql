d77ae419a6a9b63a6a1fb476cfec79de7cb9db18
Niharika Sannametla <niharika.sannametla@ing.com>
2025-11-07 10:19:11 +0000

    ok

--- macros/log_model_results.sql
+++ macros/log_model_results.sql
@@ -5,22 +5,21 @@
 {% set project_schema = var('project_schema') %}
 
 
-{# Extracting model_name, model runs status, model execution time, message(rows processed information/error message in case of failure) from results object #}
+{# Extracting model_name, model runs status, model execution time, dbt invocation_Id, dbt internal environment variable DBT_CLOUD_RUN_ID, message(rows processed information/error message in case of failure) from results object #}
     {% for result in results %}
-	  {% set model_name = result.node.name %}
+	{% set model_name = result.node.name %}
     {% set status = result.status %}
     {% set execution_time = result.execution_time | default(0) %}
     {% set run_id = invocation_id %}
     {% set cloud_run_id = env_var('DBT_CLOUD_RUN_ID',0) %}
     
 {# escaping special characters by using replace function in the model run message to avoid error while making entry to the model run table #}
-    {% set message = result.message | replace("\\", "\\\\") | replace("'", "\\'")  | replace('"', '\\"') | replace('\n', ' ') | replace('\r', ' ')  
- %}
+    {% set message = result.message | replace("\\", "\\\\") | replace("'", "\\'")  | replace('"', '\\"') | replace('\n', ' ') | replace('\r', ' ')  %}
    
     {% do run_query(
       
 "INSERT INTO `" ~ project_id ~ "." ~ project_schema ~ ".model_run_log` (
-     model_name, status, execution_time, message, run_timestamp, model_run_id, job_flow_run_id 
+     model_name, status, execution_time, message, run_timestamp, job_run_id, flow_run_id 
    ) VALUES (
      '" ~ model_name ~ "',
      '" ~ status ~ "',
