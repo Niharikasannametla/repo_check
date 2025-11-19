86dd36d92eebf772640d0275009751da72dc0297
Niharika Sannametla <niharika.sannametla@ing.com>
2025-11-19 12:50:17 +0000

    ok

--- macros/log_model_results.sql
+++ macros/log_model_results.sql
@@ -3,11 +3,11 @@
 
 {% set project_id = var('project_id') %}
 {% set project_schema = var('project_schema') %}
-
-
 {# Extracting model_name, model runs status, model execution time, dbt invocation_Id, dbt internal environment variable DBT_CLOUD_RUN_ID, message(rows processed information/error message in case of failure) from results object #}
     {% for result in results %}
-	{% set model_name = result.node.name %}
+    {% if result.node.package_name != 'dbt_project_evaluator' %}
+	
+    {% set model_name = result.node.name %}
     {% set status = result.status %}
     {% set execution_time = result.execution_time | default(0) %}
     {% set run_id = invocation_id %}
@@ -15,7 +15,6 @@
     
 {# escaping special characters by using replace function in the model run message to avoid error while making entry to the model run table #}
     {% set message = result.message | replace("\\", "\\\\") | replace("'", "\\'")  | replace('"', '\\"') | replace('\n', ' ') | replace('\r', ' ')  %}
-   
     {% do run_query(
       
 "INSERT INTO `" ~ project_id ~ "." ~ project_schema ~ ".model_run_log` (
@@ -30,5 +29,6 @@
     " ~ cloud_run_id ~ " 
    )"
     ) %}
+   {% endif %} 
   {% endfor %}
 {% endmacro %}
