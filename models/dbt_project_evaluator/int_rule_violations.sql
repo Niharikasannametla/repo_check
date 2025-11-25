cd177cf7e22ac06fecb876bbe1ec9630ca6fc4e5
Niharika Sannametla <niharika.sannametla@ing.com>
2025-11-25 09:19:30 +0000

    ok

--- models/dbt_project_evaluator/int_rule_violations.sql
+++ models/dbt_project_evaluator/int_rule_violations.sql
@@ -1,7 +1,7 @@
 select
     'Model names should be lowercase' as rule_description,
     resource_name as incorrect_model_value,
-    original_path
+    directory_path
 from {{ ref('int_all_graph_resources') }}
 where resource_type = 'model'
   and lower(resource_name) != resource_name
