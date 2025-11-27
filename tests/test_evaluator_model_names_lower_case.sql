c9da94993eb43f437d7dce1ade4ca2d980de97e1
Niharika Sannametla <niharika.sannametla@ing.com>
2025-11-27 10:13:03 +0000

    ok

--- tests/test_evaluator_model_names_lower_case.sql
+++ tests/test_evaluator_model_names_lower_case.sql
@@ -1,8 +1,14 @@
 {# This test directly queries the internal model created by the dbt_project_evaluator package.#}
 select
-    'Model names should be lowercase' as rule_description,
-    resource_name as incorrect_model_value,
-    directory_path
+    
+    resource_name as incorrect_model_name,
+    file_path as model_path,
+    'Error/Warn: Model names should be lowercase' as comment,
+    CURRENT_TIMESTAMP() as load_ts
+    
+--from {{ source('evaluator_source', 'int_all_graph_resources') }}
+
 from {{ ref('int_all_graph_resources') }}
 where resource_type = 'model'
   and lower(resource_name) != resource_name
+
